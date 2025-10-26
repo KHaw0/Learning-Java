66497ad3ef4e86b3686b63754c6a6e3dac1cd80f
ma4k <tinnaphob2550@gmail.com>
2025-10-27 00:24:23 +0700

    language

---
+++ src/language.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class language {
+    public static void main(String[] args){
+        language obj = new language();
+        String language1 = obj.Input("input Language1 >> ");
+        String language2 = obj.Input("input Language2 >> ");
+        String language3 = obj.Input("input Language3 >> ");
+        obj.Display(language1, language2, language3);
+    }
+
+    public String Input(String UI){
+        Scanner in = new Scanner(System.in);
+        System.out.print(UI);
+        return in.nextLine();
+    }
+
+    public void Display(String one, String two, String three){
+        System.out.println("==============================");
+        System.out.print("I can speak " + one + " ");
+        System.out.print("and " + two + " ");
+        System.out.println("except " + three);
+    }
+}
