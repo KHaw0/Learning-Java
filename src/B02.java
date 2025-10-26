357acf3f0e3f004579c64117230a7def0eb96182
ma4k <tinnaphob2550@gmail.com>
2025-10-27 01:04:41 +0700

    add B02, B03

---
+++ src/B02.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class B02 {
+    public static void main(String[] args){
+        B02 obj = new B02();
+        double f = obj.Input("Enter Fahrenheit (F) >> ");
+        obj.Display(f);
+    }
+
+    public double Input(String UI){
+        Scanner in = new Scanner(System.in);
+        System.out.print(UI);
+        return in.nextDouble();
+    }
+
+    public double getC(double f){
+        return (double)5 / 9 * (f - 32);
+    }
+
+    public void Display(double f){
+        System.out.println("===============================");
+        System.out.printf("%.2f Fahrenheit = %.2f Celsius", f, getC(f));
+    }
+}
---
+++ src/B03.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+
+public class B03 {
+    public static void main(String[] args){
+        B03 obj = new B03();
+        int ass = obj.Input("Assignment score >> ");
+        int mid = obj.Input("Midterm score >> ");
+        int fn = obj.Input("Final score >> ");
+        obj.Display(ass, mid, fn);
+    }
+
+    public int Input(String UI){
+        Scanner in = new Scanner(System.in);
+        System.out.print(UI);
+        return in.nextInt();
+    }
+
+    public double getAverage(int ass, int mid, int fn){
+        return ((double)ass + mid + fn) / 3;
+    }
+
+    public String getStr(int ass, int mid, int fn){
+        if(getAverage(ass, mid, fn) >= 80){
+            return "Pass";
+        }else if(getAverage(ass, mid, fn) >= 65){
+            return "Good";
+        }else if(getAverage(ass, mid, fn) >= 50){
+            return "Pass";
+        }else{
+            return "Fail";
+        }
+    }
+
+    public void Display(int ass, int mid, int fn){
+        System.out.println("==============================");
+        System.out.printf("Average score: %.2f ", getAverage(ass, mid, fn));
+        System.out.println("(" + getStr(ass, mid, fn) + ")");
+    }
+}
