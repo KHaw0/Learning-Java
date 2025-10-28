99ec84b7a1e65d30d69cdc855120f8f107b41d23
ma4k <tinnaphob2550@gmail.com>
2025-10-28 21:06:38 +0700

    Update area.java

--- src/area.java
+++ src/area.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
 
 public class area {
+    Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        int width;
-        int hight;
-        int area;
-        Scanner in = new Scanner(System.in);
-        System.out.print("Enter Width : ");
-        width = in.nextInt();
-        System.out.print("Enter Hight : ");
-        hight = in.nextInt();
-        area = hight * width;
-        System.out.println("area = " + area);
+        area obj = new area();
+        double width = obj.Input("WIDTH : ");
+        double height = obj.Input("HEIGHT : ");
+        System.out.println("AREA = " + obj.getArea(width, height));
+    }
+    public double Input(String UI){
+        System.out.print(UI);
+        return in.nextDouble();
+    }
+
+    public double getArea(double w, double h){
+        return w * h;
     }
 }
