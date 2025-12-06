3db8a49307fb187aeea1a422ef0c1588ecd1f1b9
ma4k <tinnaphob2550@gmail.com>
2025-12-06 18:25:18 +0700

    Update Ch4Ex5.java

--- src/Ch4Ex5.java
+++ src/Ch4Ex5.java
@@ -4,12 +4,8 @@ public class Ch4Ex5 {
     static void main(String[] args) {
         Random rn = new Random();
         for (int i = 0; i < 10; i++) {
-            char num = (char)rn.nextInt();
-            if(num >= 65 && num <= 90){
-                System.out.println(num);
-            }else{
-                i--;
-            }
+            char num = (char)rn.nextInt(65, 90);
+            System.out.println(num);
         }
     }
 }
