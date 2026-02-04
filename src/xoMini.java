bc9d0c955a30b9aeb7ad6d74cedcbfaba6085ba5
ma4k <tinnaphob2550@gmail.com>
2026-02-04 16:06:56 +0700

    Update xoMini.java

--- src/xoMini.java
+++ src/xoMini.java
@@ -2,36 +2,12 @@ import java.util.Arrays;
 import java.util.Scanner;
 class xoMini {
     public static void main(String[] args) {
-        xo obj = new xo();
+        xoMini obj = new xoMini();
         int n = Integer.parseInt(obj.input("TableSize(NxN): "));
         char[][] table = new char[n][n];
-        boolean[][] used = new boolean[n][n];
         obj.setTable(table);
         obj.printTable(table);
-        for(int i = 0; i < table.length * table.length; i++){
-            char ch = obj.input("input X/O: ").charAt(0);
-            if(ch == 'E'){
-                break;
-            }
-            int indexI = Integer.parseInt(obj.input("Index i: "));
-            int indexJ = Integer.parseInt(obj.input("Index j: "));
-            if(ch == 'X' || ch == 'O' || ch == 'x' || ch == 'o'){
-                if(indexI >= 0 && indexI < table.length && indexJ >= 0 && indexJ < table.length){
-                    if(!used[indexI][indexJ]){
-                        used[indexI][indexJ] = true;
-                        obj.printTable(table);
-                    }else{
-                        i--;
-                        System.out.println("INDEX Already");
-                    }
-                }else {
-                    System.out.println("INDEX ERROR");
-                }
-            }else {
-                System.out.println("pls input X/O");
-            }
-
-        }
+        obj.game(table);
     }
 
     String input(String UI){
@@ -55,4 +31,37 @@ class xoMini {
         }
         System.out.println();
     }
+
+	void setXO(char[][] table, char ch, int i, int j){
+        table[i][j] = ch;
+    }
+
+	void game(char[][] table){
+		boolean[][] used = new boolean[table.length][table.length];
+		for(int i = 0; ; i++){
+            char ch = input("input X/O: ").charAt(0);
+            if(ch == 'E'){
+                break;
+            }
+            int indexI = Integer.parseInt(input("Index i: "));
+            int indexJ = Integer.parseInt(input("Index j: "));
+            if(ch == 'X' || ch == 'O'){
+                if(indexI >= 0 && indexI < table.length && indexJ >= 0 && indexJ < table.length){
+                    if(!used[indexI][indexJ]){
+                        used[indexI][indexJ] = true;
+						setXO(table, ch, indexI, indexJ);
+                        printTable(table);
+                    }else{
+                        i--;
+                        System.out.println("INDEX Already Used!");
+                    }
+                }else {
+                    System.out.println("INDEX ERROR!");
+                }
+            }else {
+                System.out.println("pls input X/O");
+            }
+			if(i == table.length * table.length - 1) break;
+        }
+	}
 }
